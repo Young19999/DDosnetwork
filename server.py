5d6a8753782e50468c56d599c74df3a1cc270cc7
S1ng13 <1134283663@qq.com>
2024-12-24 14:53:22 +0800

    server变量确定

--- server.py
+++ server.py
@@ -1,5 +1,28 @@
+import threading
+
+class netbot:
+    def __init__(self, id, is_start, ip_address):
+        self.id = id
+        self.is_start = is_start
+        self.ip_address = ip_address
+
 
 class Server:
+    def __init__(self):
+        self.socketfd = -1
+
+        self.attacking = False
+        self.attack = "None"
+
+        self.target_ip = "None"
+        self.target_port = 0
+
+        self.netbots = []
+        self.tListener = threading.Thread()
+
+    def get_server_fd(self):
+        return self.socketfd
+
     def server_status():
         
     def setup_target_parameters():
