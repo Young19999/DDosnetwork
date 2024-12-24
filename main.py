4eb800e23bbb66e893ee34641e4594bd53f79a82
t1mmuz <1423766572@qq.com>
2024-12-24 14:23:12 +0800

    architecture

---
+++ main.py
@@ -0,0 +1,9 @@
+import server
+
+def start_server():
+    
+def main_menu():
+
+def signal_callback_handler():
+    
+def main():
---
+++ server.py
@@ -0,0 +1,18 @@
+
+class Server:
+    def server_status():
+        
+    def setup_target_parameters():
+        
+    def start_attack():
+        
+    def close_connected_sockets():
+    
+    def list_bots():
+        
+    def setup_attack_type():
+        
+    def threaded():
+    
+    def connection_listener():    
+    
